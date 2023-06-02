7c82f01d5d1d480021dc3687a1192b27afd9214f
Socks aka Janek <jan.kwiecien06@gmail.com>
2023-06-02 18:19:34 +0200

    :rocket: never awaited fixed but now there's new bug
    btw how tf yield, corutines, async and all this this works. I don't have fucking clue

--- Scraping/scraping_parsing.py
+++ Scraping/scraping_parsing.py
@@ -1,15 +1,13 @@
-#szklanki id: 18869
-#talerzyki id: 
-import ikea_api, json
+import ikea_api, json, asyncio
 
 constants = ikea_api.Constants(country="us", language="en")
 
-endpoint = ikea_api.get_items(constants, ["30457903"])
+async def get_this_shit():
+    item = await ikea_api.get_items(constants, ["30457903"])
+    print(item)
 
-item = ikea_api.run(endpoint)
+item = asyncio.run(get_this_shit())
+
 item_json = json.dumps(item, indent=4)
 
-print(item_json)
-
-#EndpointInfo(func=functools.partial(<function PipItem.get_item at 0x000002D167842D40>, <ikea_api.endpoints.pip_item.PipItem object at 0x000002D16726C610>, '30288241'), handlers=())
-#EndpointInfo(func=functools.partial(<function IowsItems.get_items at 0x000001C23807E480>, <ikea_api.endpoints.iows_items.IowsItems object at 0x000001C238918990>, '30288241'), handlers=())
+print(item)
