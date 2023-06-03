1822253d18f840efe3c0bae765906c51ca717ef0
codingTheRedCat <jan.alex.strakowski@gmail.com>
2023-06-03 15:42:21 +0200

    this shit does not work, please help me

--- Scraping/scraping_parsing.py
+++ Scraping/scraping_parsing.py
@@ -1,15 +1,13 @@
 #szklanki id: 18869
 #talerzyki id: 
-import ikea_api, json
+import ikea_api, json, asyncio
 
-constants = ikea_api.Constants(country="us", language="en")
+constants = ikea_api.Constants(country="ru", language="pl")
 
-endpoint = ikea_api.get_items(constants, ["30457903"])
+items = asyncio.run(ikea_api.get_items(constants, ["30288241"]))
+#item_json = json.dumps(item, indent=4)
 
-item = await ikea_api.run_async(endpoint)
-item_json = json.dumps(item, indent=4)
-
-print(item_json)
+print(item)
 
 #EndpointInfo(func=functools.partial(<function PipItem.get_item at 0x000002D167842D40>, <ikea_api.endpoints.pip_item.PipItem object at 0x000002D16726C610>, '30288241'), handlers=())
 #EndpointInfo(func=functools.partial(<function IowsItems.get_items at 0x000001C23807E480>, <ikea_api.endpoints.iows_items.IowsItems object at 0x000001C238918990>, '30288241'), handlers=())
